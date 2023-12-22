632aaf4a09ec7b5d3ab3163305e49d5b9afe3cbf
ZenBowman <PrasanSamtani@gmail.com>
2023-12-22 05:13:26 -0800

    Catsim2, this time using Apple's SpriteKit

--- catsim2/GameScene.swift
+++ catsim2/GameScene.swift
@@ -16,7 +16,7 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        self.label = self.childNode(withName: "//catsimLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
@@ -75,7 +75,7 @@ class GameScene: SKScene {
     
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
-        case 0x31:
+        case 0x31:  // Pulse on Spacebar
             if let label = self.label {
                 label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
             }
